53faa3a0ae1c123b7c9086ceeed230600b2e359d
G4st3r21 <70285259+G4st3r21@users.noreply.github.com>
2021-02-02 20:24:21 +0300

    vscode settings

--- main.py
+++ main.py
@@ -19,24 +19,32 @@ lon = args.first_coord[0]
 lat = args.second_coord[0]
 delta = args.delta[0]
 
-print(lon, lat)
-
-params = {
-    "ll": ",".join([lon, lat]),
-    "spn": ",".join([delta, delta]),
-    "l": "map"
-}
-
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
 
 while pygame.event.wait().type != pygame.QUIT:
+    params = {
+        "ll": ",".join([lon, lat]),
+        "spn": ",".join([delta, delta]),
+        "l": "map"
+    }
+
     response = requests.get(api_server, params=params)
 
     map_file = "map.png"
     with open(map_file, "wb") as file:
         file.write(response.content)
-    
+
+    pressed_key = pygame.key.get_pressed()
+    if pressed_key[pygame.K_UP]:
+        lon = str(float(lon) + 0.005)
+    if pressed_key[pygame.K_DOWN]:
+        lon = str(float(lon) - 0.005)
+    if pressed_key[pygame.K_LEFT]:
+        lat = str(float(lat) - 0.005)
+    if pressed_key[pygame.K_RIGHT]:
+        lat = str(float(lat) + 0.005)
+
     screen.blit(pygame.image.load(map_file), (0, 0))
     pygame.display.flip()
 pygame.quit()
