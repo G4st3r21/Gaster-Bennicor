35d7e551e6181264fe89a6d2513b71bbb4c35780
G4st3r21 <70285259+G4st3r21@users.noreply.github.com>
2021-02-02 19:02:15 +0300

    1 задача

---
+++ WEB. Решение задач на API Яндекс.Карт/1.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+import pygame
+import requests
+
+api_server = "http://static-maps.yandex.ru/1.x/"
+
+lon = "37.530887"
+lat = "55.703118"
+delta = "0.002"
+
+params = {
+    "ll": ",".join([lon, lat]),
+    "spn": ",".join([delta, delta]),
+    "l": "map"
+}
+response = requests.get(api_server, params=params)
+
+map_file = "map.png"
+with open(map_file, "wb") as file:
+    file.write(response.content)
+
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+pygame.quit()
+
+os.remove(map_file)
