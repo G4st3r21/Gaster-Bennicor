d42c509c420936eec7e51dadcbf13d349d08ccdb
Vlad Boldyrev <boldyrev2040@gmail.com>
2021-02-03 19:27:04 +0300

    Решение 4 задачи

--- main.py
+++ main.py
@@ -1,54 +1,99 @@
 import os
 import sys
-
 import pygame
 import requests
 import argparse
+from itertools import cycle
+from button import Button
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument("first_coord", nargs='+')
-parser.add_argument("second_coord", nargs='+')
-parser.add_argument("delta", nargs='+')
+parser.add_argument("first_coord")
+parser.add_argument("second_coord")
+parser.add_argument("scale")
 args = parser.parse_args()
 
-api_server = "http://static-maps.yandex.ru/1.x/"
-
-# lon = "37.530887"
-# lat = "55.703118"
-# delta = "0.002"
-
-lon = args.first_coord[0]
-lat = args.second_coord[0]
-delta = args.delta[0]
-
+# params 37.530887 55.703118 15
 pygame.init()
-screen = pygame.display.set_mode((600, 450))
+font = pygame.font.Font(None, 25)
+map_file = "map.png"
+maps = cycle(["map", "sat", "sat,skl"])
+titles = cycle(["Схема", "Спутник", "Гибрид"])
 
-while pygame.event.wait().type != pygame.QUIT:
-    params = {
-    "ll": ",".join([lon, lat]),
-    "spn": ",".join([delta, delta]),
-    "l": "map"
-    }
+api_server = "http://static-maps.yandex.ru/1.x/"
+lon = str(args.first_coord)
+lat = str(args.second_coord)
+z = int(args.scale)
+l = next(maps)
 
-    response = requests.get(api_server, params=params)
+scale_step = 1
+coords_step = 0.005
 
-    map_file = "map.png"
-    with open(map_file, "wb") as file:
-        file.write(response.content)
-    
+screen = pygame.display.set_mode((600, 500))
+map_button = Button(screen, 0, 450, 150, 50, next(titles), font)
+
+click = False
+changes_made = True
+running = True
+
+while running:
     pressed_key = pygame.key.get_pressed()
-    if pressed_key[pygame.K_UP]:
-        lon = str(float(lon) + 0.005)
-    if pressed_key[pygame.K_DOWN]:
-        lon = str(float(lon) - 0.005)
-    if pressed_key[pygame.K_LEFT]:
-        lat = str(float(lat) - 0.005)
-    if pressed_key[pygame.K_RIGHT]:
-        lat = str(float(lat) + 0.005)
+    click = False
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_PAGEDOWN and z >= 1:
+                z -= scale_step
+                changes_made = True
+            elif event.key == pygame.K_PAGEUP and z <= 21:
+                z += scale_step
+                changes_made = True
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                click = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                click = False
 
-    screen.blit(pygame.image.load(map_file), (0, 0))
+    if pressed_key[pygame.K_UP]:
+        lat = str(float(lat) + coords_step)
+        changes_made = True
+    if pressed_key[pygame.K_DOWN]:
+        lat = str(float(lat) - coords_step)
+        changes_made = True
+    if pressed_key[pygame.K_LEFT]:
+        lon = str(float(lon) - coords_step)
+        changes_made = True
+    if pressed_key[pygame.K_RIGHT]:
+        lon = str(float(lon) + coords_step)
+        changes_made = True
+
+    map_button.update()
+
+    x, y = pygame.mouse.get_pos()
+    if map_button.collide((x, y)) and click:
+        map_button.set_text(next(titles))
+        l = next(maps)
+        changes_made = True
+
+    if changes_made:
+        params = {
+            "ll": ",".join([lon, lat]),
+            "z": z,
+            "l": l
+        }
+
+        response = requests.get(api_server, params=params)
+
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+    
+        screen.blit(pygame.image.load(map_file), (0, 0))
+        
+    changes_made = False
     pygame.display.flip()
 pygame.quit()
-
 os.remove(map_file)
